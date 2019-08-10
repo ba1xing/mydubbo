bb513c2639d3b3c8c6142425eaf51403289f166f
baixing <ba1xing@163.com>
2019-08-10 17:18:02 +0800

    mydubbo

--- server-model/src/main/java/cn/wellcare/model/User.java
+++ server-model/src/main/java/cn/wellcare/model/User.java
@@ -1,27 +1,14 @@
 package cn.wellcare.model;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
-//@Data
+@Data
 public class User implements Serializable {
 
     private String id;
 
     private String name;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
